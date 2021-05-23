a33be638363ac293b742eb4e943e45dce3ebbb6d
huypc <huy8895@gmail.com>
2021-05-23 16:27:44 +0700

    config redis

---
+++ src/main/java/com/huy8895/dictionaryapi/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.huy8895.dictionaryapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        return new JedisConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379));
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(){
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory());
+        return template;
+    }
+}
