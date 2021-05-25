47cb63edef8a5597f72e0ddddec5903faa7f48f0
huypc <huy8895@gmail.com>
2021-05-26 06:59:54 +0700

    redis: set redis expired time

--- src/main/java/com/huy8895/dictionaryapi/service/impl/WordServiceImpl.java
+++ src/main/java/com/huy8895/dictionaryapi/service/impl/WordServiceImpl.java
@@ -5,14 +5,17 @@ import com.huy8895.dictionaryapi.enity.Word;
 import com.huy8895.dictionaryapi.repository.WordRepository;
 import com.huy8895.dictionaryapi.service.WordService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WordServiceImpl implements WordService {
     private final WordRepository wordRepository;
     private final RedisTemplate<String, String> redisTemplate;
@@ -27,15 +30,18 @@ public class WordServiceImpl implements WordService {
 
     @Override
     public Word get(Long id) {
+        log.info("get word in redis: {}", id );
         String o = (String) redisTemplate.opsForHash()
                                          .get(HASH_KEY, String.valueOf(id));
         if (o == null) {
+            log.info("get word in wordRepository: {}", id );
             Optional<Word> repositoryOne = wordRepository.findById(id);
             if (repositoryOne.isPresent()){
                 Word word = repositoryOne.get();
                 String s = gson.toJson(word);
                 redisTemplate.opsForHash()
                              .put(HASH_KEY, String.valueOf(id), s);
+                redisTemplate.expire(HASH_KEY,10000, TimeUnit.MILLISECONDS);
                 return word;
             }
 
